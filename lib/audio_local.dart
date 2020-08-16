5568912c429110cf95d8ec93c325c9385ccf76d4
Aman Kedia <amankedia1402@gmail.com>
2020-08-17 04:07:31 +0530

    list view and bottom nav bar

--- lib/audio_local.dart
+++ lib/audio_local.dart
@@ -14,97 +14,103 @@ class AudioLocal extends StatelessWidget {
           width: MediaQuery.of(context).size.width * 1.0,
           height: MediaQuery.of(context).size.height * 1.0,
           color: Colors.lightBlue.shade50,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
+            padding: const EdgeInsets.all(8),
             children: <Widget>[
               Container(
-                width: MediaQuery.of(context).size.width * 1.0,
-                height: MediaQuery.of(context).size.height * 0.095,
-                color: Colors.transparent,
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.33,
-                      height: MediaQuery.of(context).size.height * 1.0,
-                      color: Colors.transparent,
-                      child: FlatButton(
-                        onPressed: () {
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) => HomeScreen(),
-                            ),
-                          );
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              Icons.home,
-                              color: Colors.black54,
-                              size: MediaQuery.of(context).size.height * 0.058,
-                            ),
-                            Text('Home'),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.33,
-                      height: MediaQuery.of(context).size.height * 1.0,
-                      color: Colors.transparent,
-                      child: FlatButton(
-                        onPressed: null,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              Icons.queue_music,
-                              color: Colors.blue.shade800,
-                              size: MediaQuery.of(context).size.height * 0.058,
-                            ),
-                            Text(
-                              'Local',
-                              style: TextStyle(
-                                color: Colors.blue.shade800,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.34,
-                      height: MediaQuery.of(context).size.height * 1.0,
-                      color: Colors.transparent,
-                      child: FlatButton(
-                        onPressed: () {
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) => AudioOnline(),
-                            ),
-                          );
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              Icons.library_music,
-                              color: Colors.black54,
-                              size: MediaQuery.of(context).size.height * 0.058,
-                            ),
-                            Text('Online'),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
+                height: 50,
+                color: Colors.amber[600],
+                child: const Center(child: Text('Audio Local')),
+              ),
             ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 1.0,
+            height: MediaQuery.of(context).size.height * 0.095,
+            color: Colors.lightBlue.shade50,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.33,
+                  height: MediaQuery.of(context).size.height * 0.095,
+                  color: Colors.transparent,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => HomeScreen(),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.home,
+                          color: Colors.black54,
+                          size: MediaQuery.of(context).size.height * 0.058,
+                        ),
+                        Text('Home'),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.33,
+                  height: MediaQuery.of(context).size.height * 0.095,
+                  color: Colors.transparent,
+                  child: FlatButton(
+                    onPressed: null,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.queue_music,
+                          color: Colors.blue.shade800,
+                          size: MediaQuery.of(context).size.height * 0.058,
+                        ),
+                        Text(
+                          'Local',
+                          style: TextStyle(
+                            color: Colors.blue.shade800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.34,
+                  height: MediaQuery.of(context).size.height * 0.095,
+                  color: Colors.transparent,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => AudioOnline(),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.library_music,
+                          color: Colors.black54,
+                          size: MediaQuery.of(context).size.height * 0.058,
+                        ),
+                        Text('Online'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
